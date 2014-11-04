e0f20b9b5a5a870290162de6dd43665eb8b54178
Liam Howell <lhowell@mobiquityinc.com>
2014-11-04 15:59:35 -0500

    Added module skeleton

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Holds utility methods for interacting with an existing Gruntfile.js.
+ *
+ * Author:	Liam Howell <lhowell@mobiquityinc.com>
+ * Since:	11-04-2014
+ */
+module.export = {
+	extractJSON: function (gruntfileObject, configToExtract) {
+		console.log(gruntfileObject);
+	}
+}
